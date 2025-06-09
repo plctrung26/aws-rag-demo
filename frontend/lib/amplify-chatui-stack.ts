8d630947e81b1db7c344a0e2c7e99f0bb0dab689
Trung Phan Lê Chí <21522725@ms.uit.edu.vn>
2025-06-09 23:42:55 +0700

    fix bugs

--- frontend/lib/amplify-chatui-stack.ts
+++ frontend/lib/amplify-chatui-stack.ts
@@ -1,15 +1,12 @@
-import * as amplify from '@aws-cdk/aws-amplify-alpha';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ssm from 'aws-cdk-lib/aws-ssm';
-import path = require('path');
+import * as amplify from '@aws-cdk/aws-amplify-alpha';
+import { GitHubSourceCodeProvider } from '@aws-cdk/aws-amplify-alpha';
 
 export class AmplifyChatuiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // -------------------------------------------------------------------------
-    // Load SSM parameters
 
     const cognito_user_pool_id_parameter = ssm.StringParameter.valueForStringParameter(
       this, "/AgenticLLMAssistant/cognito_user_pool_id"
@@ -23,29 +20,25 @@ export class AmplifyChatuiStack extends cdk.Stack {
       this, "/AgenticLLMAssistant/agent_api"
     );
 
-    // -------------------------------------------------------------------------
-    // Deploy using local build output (no CodeCommit needed)
-
     const amplifyChatUI = new amplify.App(this, 'AmplifyChatUI', {
       autoBranchDeletion: true,
-      sourceCodeProvider: new amplify.AssetSourceCodeProvider({
-        asset: cdk.aws_s3_assets.Asset.fromPath(path.join(__dirname, '../../frontend/out')), // or 'build' or '.next'
+      sourceCodeProvider: new GitHubSourceCodeProvider({
+        owner: 'plctrung26',
+        repository: 'aws-rag-demo',
+        oauthToken: cdk.SecretValue.secretsManager('my-github-token'),
       }),
-      platform: amplify.Platform.WEB_COMPUTE, // SSR support
+      platform: amplify.Platform.WEB_COMPUTE,
       environmentVariables: {
         '_CUSTOM_IMAGE': 'amplify:al2023',
         'AMPLIFY_USERPOOL_ID': cognito_user_pool_id_parameter,
         'COGNITO_USERPOOL_CLIENT_ID': cognito_user_pool_client_id_parameter,
-        'API_ENDPOINT': agent_api_parameter
+        'API_ENDPOINT': agent_api_parameter,
       },
     });
 
     amplifyChatUI.addBranch('main', {
-      stage: "PRODUCTION",
+      stage: 'PRODUCTION',
     });
-
-    // -----------------------------------------------------------------------
-    // Outputs
 
     new cdk.CfnOutput(this, "AmplifyAppURL", {
       value: amplifyChatUI.defaultDomain,
